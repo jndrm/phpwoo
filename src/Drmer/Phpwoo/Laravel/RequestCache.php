cd4e161bb8606eb26f6276135e320f2036b9c84c
Donald <donald@drmer.net>
2019-07-16 06:51:28 +0800

    read response header to determine cache duration

--- src/Drmer/Phpwoo/Laravel/RequestCache.php
+++ src/Drmer/Phpwoo/Laravel/RequestCache.php
@@ -6,27 +6,28 @@ use Cache;
 
 class RequestCache
 {
-	public function get($req)
+    public function get($req)
     {
         $server = $req->server;
-        if (strtolower($server['request_method']) != 'get') {
-            return null;
+        $method = strtolower($server['request_method']);
+        if (!in_array($method, ['get', 'head'])) {
+            return false;
         }
         $cacheKey = md5($server['request_uri']);
-        $content = Cache::get($cacheKey);
-        if ($content) {
-            return unserialize($content);
-        }
-        return null;
+        return Cache::get($cacheKey);
     }
 
     public function should($req, $res)
     {
         $server = $req->server;
-        if (strtolower($server['request_method']) != 'get') {
+        $method = strtolower($server['request_method']);
+        if (!in_array($method, ['get', 'head'])) {
             return false;
         }
         if ($res['status'] != 200) {
+            return false;
+        }
+        if ($res['headers']->getCacheControlDirective('private')) {
             return false;
         }
         return true;
@@ -34,8 +35,14 @@ class RequestCache
 
     public function put($req, $res)
     {
+        $maxAge = $res['headers']->getCacheControlDirective('max-age');
+        if ($maxAge <= 0) {
+            return;
+        }
+        $res['headers']->remove('set-cookie');
+
         $key = md5($req->server['request_uri']);
-        echo "caching $key\n";
-        Cache::put($key, serialize($res), 10);
+        $expiresAt = now()->addSeconds($maxAge);
+        Cache::put($key, $res, $expiresAt);
     }
 }
